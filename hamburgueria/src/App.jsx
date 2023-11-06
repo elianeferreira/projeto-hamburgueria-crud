4d92f75cbfa397122e95ba50c55f5bae9d7b4b76
Flaviana <flaviferraz@yahoo.com.br>
2023-11-05 22:31:37 -0300

    criação do componente Header

--- hamburgueria/src/App.jsx
+++ hamburgueria/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import Router from './Router'
+import Header from './components/Header'
 
 function App() {
  
   return (
     <div className='App'>
        <Router/>
-
+      <Header />
     </div>
     
   )
---
+++ hamburgueria/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import styles from '../styles/components/Header.module.css'
+import * as Icon from '@phosphor-icons/react';
+
+const Header = () => {
+    return(
+         <div className={styles.headerContainer}>
+            <div className={styles.headerRight}>
+                <img className={styles.logo} src="" alt='logo-hamburgueria'/>
+                <nav className={styles.headerNav}>
+                    <h2>Início</h2>
+                    <h2>Cadastro</h2>
+                    <h2>Equipe</h2>
+                </nav>
+            </div>
+            
+            <div className={styles.headerSocialMedia}>
+                <Icon.FacebookLogo size={32} style={{color: '#ffffff'}} alt='facebook-logo' />
+                <Icon.InstagramLogo size={32} style={{color:'#ffffff'}} alt='intagram-logo' />
+                <Icon.TwitterLogo size={32}  style={{color:'#ffffff'}} alt='twitter-logo' />
+                <Icon.LinkedinLogo size={32} style={{color:'#ffffff'}} alt='linkedin-logo' />
+            </div>
+        </div>
+    )
+}
+
+export default Header;
