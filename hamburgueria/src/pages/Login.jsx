c330a0093cc0818072c8e2e3cfbc60a5e77ef4e9
elianeferreira <sanelisantos@hotmail.com>
2023-11-20 19:29:39 -0300

    inclusao pages login e styles

--- hamburgueria/src/pages/Login.jsx
+++ hamburgueria/src/pages/Login.jsx
@@ -4,35 +4,52 @@ import HeaderLogin from'../components/HeaderLogin'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Styles from '../Styles/components/Login.module.css'
-
+import { useState } from 'react'
 
 
 const Login = () => {
+  const [Email, SetEmail] = useState ('')
+  const [Senha, SetSenha] = useState ('')
+  
+  function handleInputEmail(e){
+    SetEmail(e.target.value)
+  }
+  function handleInpuSenha(e){
+    SetSenha(e.target.value)
+  }
+  
   return (
     <>
-       <div className={Styles.LoginContainer}>
-      <h1>logo</h1>
+       <div className={Styles.Menu}>
+      
        </div>
-
-      <Header
-      image={logo}
-      />
-     <form onSubmit={()=>{}}>
-      <input 
+     
+     <div className={Styles.Container}>
+          <h1 className={Styles.h1}>AQUI</h1>
+     </div>
+    
+     <div className={Styles.containerFormulario}>
+     
+     <form className={Styles.formulario}onSubmit={()=>{}}>
+      <h2 >Faça seu Login</h2>
+      <input className={Styles.formularioInput} 
       type='text'
       placeholder="Email"
-      onChange={()=>{}}
-      value=""  
+      onChange={handleInputEmail}
+      value={Email}  
       />
 
-      <input 
+      <input className={Styles.formularioInput} 
       type='text'
       placeholder="Senha"
-      onChange={()=>{}}
-      value=""
+      onChange={handleInpuSenha}
+      value={Senha}
       /> 
-     <button type="submit">Entrar</button>
+     <button className={Styles.buttonFormulario} type="submit">Entrar</button>
     </form>
+
+     </div>
+     
     </>
       
   )  
