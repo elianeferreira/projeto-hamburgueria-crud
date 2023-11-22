b0aa72ce22e152ac1af0cba8d8caf97c4e4cd1ed
elianeferreira <sanelisantos@hotmail.com>
2023-11-21 22:01:30 -0300

    cardapio inclusao

--- hamburgueria/src/components/CardCardapio.jsx
+++ hamburgueria/src/components/CardCardapio.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import styles from '../Styles/components/CardCardapio.module.css'
 
 const CardCardapio = ({image, title, hamburguer, ingredientes , Preço}) => {
   return (
 
-    <>
-    <img src={image} alt={title} />
-    <h1>{hamburguer}</h1>
-    <p>{ingredientes}</p>
-    <h2>{Preço}</h2>
-    </>
+    <div className={styles.CardCardapio} >
+      <img className={styles.imghamburguer} src={image} alt={title} />
+        <h1 className={styles.NomeHamburguer}>{hamburguer}</h1>
+        <p className={styles.ingredientes}>{ingredientes}</p>
+        <h2 className={styles.preco}>{Preço}</h2>
+    </div>
+    
+    
 
    
   )
--- hamburgueria/src/pages/Cardapio.jsx
+++ hamburgueria/src/pages/Cardapio.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Header from '../components/Header'
 import LateralBar from '../components/LateralBar'
-import styles from '../styles/pages/Cardapio.module.css'
+import styles from '../Styles/pages/Cardapio.module.css'
 import CardCardapio from '../components/CardCardapio'
 import Logo from '../assets/Logo.png'
+import hamburguer from '../assets/hamburguer1.jpg'
+
 
 
 const Cardapio = () => {
@@ -12,9 +14,11 @@ const Cardapio = () => {
       <Header />
       <div className={styles.main}>
             <LateralBar />
-      <div>
+    <div className={styles.principal}>
+
       <h1 className={styles.mainTitle}>CARDÁPIO</h1>
-      <CardCardapio image={Logo}
+      <div className={styles.cardapio}>
+      <CardCardapio image={hamburguer}
       hamburguer="HtmlBurguer"
       descrição="Lorem"
       Preço="12,00"
@@ -41,9 +45,10 @@ const Cardapio = () => {
       descrição="Lorem"
       Preço="12,00"
       />
-
-     </div>
-    </div>
+      </div>
+      </div>
+      </div>
+    
       
     </>
   )
